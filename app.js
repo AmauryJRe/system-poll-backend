901e7404f0ee25b93c9d235f6acdbba4aa7f569b
amauryjr <amauryjreynaldo@gmail.com>
2020-09-15 21:22:14 -0400

    Set Up Server for PollApi

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 
+const pollRouter = require('./routes/PollRoutes');
+
+mongoose.connect('mongodb://localhost:27017/system-poll', {
+    useNewUrlParser: true
+}).then(res => {
+    console.log('Connected to Database');
+}).catch(err => {
+    console.error(err);
+});
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 app.listen(SERVER_PORT, () => console.log(`Server running in ${SERVER_PORT}`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/',(req,res) =>{
-res.send('Hello World!');
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
+
+app.use('/poll',pollRouter);
