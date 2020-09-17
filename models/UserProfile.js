aa50ded0e207325fb6b93a2a389b25bd5969a754
amauryjr <amauryjreynaldo@gmail.com>
2020-09-17 00:13:22 -0400

    username added to model

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const userProfileSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
   name: String,
   fullName: String,
   password: String,
