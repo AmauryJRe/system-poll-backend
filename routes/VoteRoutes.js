6470e7df433d8663ff9e186b3489cfba2d2bf251
amauryjr <amauryjreynaldo@gmail.com>
2020-09-16 21:17:14 -0400

    Fixed Vote update function

--- routes/VoteRoutes.js
+++ routes/VoteRoutes.js
@@ -8,7 +8,7 @@ router.post('/', async (req, res) => {
     const vote = await voteRegistry.find({ user_id: req.body.user_id, poll_id: req.body.poll_id, })
     if (vote.length > 0 && vote[0].canVote) {
         console.log('This User can vote again, the flag is true');
-        updateVote(req, res, poll, vote[0]);
+        updateVote(req, res, poll, vote[0].id);
     }
     if (vote.length === 0) {
         console.log('This User can vote again');
@@ -34,14 +34,19 @@ const makeVote = async (req, res, poll) => {
     }
 }
 
-const updateVote = async (req, res, poll, vote) => {
+const updateVote = async (req, res, poll, vote_id) => {
     try {
+        vote = req.body;
         vote.canVote=false
-        console.log(vote);
-        await voteRegistry.findByIdAndUpdate(vote.id,vote)
-        await vote.save();
+        const voteBefore = await voteRegistry.findById(vote_id);
+        const itemBefore = voteBefore.item_voted;
         poll.options[`${req.body.item_voted}`] += 1;
-        await pollModel.findByIdAndUpdate(req.body.poll_id, poll);
+        if(poll.options[itemBefore]){
+            poll.options[itemBefore]-=1;
+        }
+        await voteRegistry.findByIdAndUpdate(vote_id,vote);
+        await pollModel.findByIdAndUpdate(vote.poll_id, poll);
+        await vote.save();
         await pollModel.save();
         res.status(200).json({ message: 'User has voted', code: 200 });
     } catch (err) {
