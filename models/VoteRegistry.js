7f7d3fa34e9bd19109d80a26cd65cc2113df2843
amauryjr <amauryjreynaldo@gmail.com>
2020-09-17 00:13:49 -0400

    Apply EsLint

--- models/VoteRegistry.js
+++ models/VoteRegistry.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const voteSchema = mongoose.Schema({
-    user_id: {
-        type: mongoose.Schema.Types.String,
-    },
-    poll_id: {
-        type: mongoose.Schema.Types.String,
-    },
-    item_voted: {
-        type: mongoose.Schema.Types.String,
-    },
-    canVote: {
-        type: mongoose.Schema.Types.Boolean,
-        default: false
-    }
+  user_id: {
+    type: mongoose.Schema.Types.String,
+  },
+  poll_id: {
+    type: mongoose.Schema.Types.String,
+  },
+  item_voted: {
+    type: mongoose.Schema.Types.String,
+  },
+  canVote: {
+    type: mongoose.Schema.Types.Boolean,
+    default: false,
+  },
 });
 
 const VoteRegistry = mongoose.model('Vote', voteSchema);
