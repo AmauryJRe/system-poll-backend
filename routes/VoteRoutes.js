833ed221ecbbf1762e13f428128c77792826ba55
amauryjr <amauryjreynaldo@gmail.com>
2020-09-16 23:22:08 -0400

    Apply EsLint

--- routes/VoteRoutes.js
+++ routes/VoteRoutes.js
@@ -6,28 +6,27 @@ const Logger = require('../models/Logger');
 const { updateVote, makeVote, resetVotesForPoll } = require('../util/voteHandler');
 const { logger } = new Logger();
 
-
 router.post('/', async (req, res) => {
-    const poll = await pollModel.findById(req.body.poll_id);
-    const vote = await voteRegistry.find({ user_id: req.body.user_id, poll_id: req.body.poll_id, })
-    if (vote.length > 0 && vote[0].canVote) {
-        logger.info(`The user: ${'user'} can vote again, the option canVote is true`);
-        updateVote(req, res, poll, vote[0].id);
-    }
-    if (vote.length === 0) {
-        logger.info(`The user: ${'user'} has no vote yet, so he/she can vote`);
-        makeVote(req, res, poll);
-    }
+  const poll = await pollModel.findById(req.body.poll_id);
+  const vote = await voteRegistry.find({ user_id: req.body.user_id, poll_id: req.body.poll_id });
+  if (vote.length > 0 && vote[0].canVote) {
+    logger.info(`The user: ${'user'} can vote again, the option canVote is true`);
+    updateVote(req, res, poll, vote[0].id);
+  }
+  if (vote.length === 0) {
+    logger.info(`The user: ${'user'} has no vote yet, so he/she can vote`);
+    makeVote(req, res, poll);
+  }
 
-    if (vote.length > 0 && !vote[0].canVote) {
-        console.log(`The user: ${'user'} can\'t vote again`);
-        res.status(403).json({ message: "This User can\'t vote again", code: 403, error: true });
-    }
+  if (vote.length > 0 && !vote[0].canVote) {
+    console.log(`The user: ${'user'} can\'t vote again`);
+    res.status(403).json({ message: "This User can't vote again", code: 403, error: true });
+  }
 });
 
-router.get('/',async (req,res,next)=>{
-    resetVotesForPoll(req,res,req.body.poll_id);
-    next();
+router.get('/', async (req, res, next) => {
+  resetVotesForPoll(req, res, req.body.poll_id);
+  next();
 });
 
 module.exports = router;
