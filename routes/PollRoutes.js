bb9d65052258ca9331401b6a469f46346bcbd8f1
amauryjr <amauryjreynaldo@gmail.com>
2020-09-21 13:37:35 -0400

    Prettify responses and Poll Patch fixed

--- routes/PollRoutes.js
+++ routes/PollRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const pollModel = require('../models/Poll');
 const router = express.Router();
+const { success, error } = require('../util/responseApi');
 const Logger = require('../models/Logger');
 const { logger } = new Logger();
+
 router.get('/', async (req, res) => {
   const polls = await pollModel.find({});
-
   try {
     res.send(polls);
   } catch (err) {
-    res.status(500).send(err);
+    res.status(500).json(error('Internal Server Error', res.statusCode));
   }
 });
 
@@ -19,18 +20,19 @@ router.get('/:id', async (req, res) => {
   try {
     res.send(poll);
   } catch (err) {
-    res.status(500).send(err);
+    res.status(500).json(error('Internal Server Error', res.statusCode));
   }
 });
 
 router.post('/', async (req, res) => {
   const poll = new pollModel({ name: req.body.name, options: JSON.parse(req.body.options) });
+  logger.info(JSON.parse(req.body.options));
 
   try {
     await poll.save();
-    res.send(poll);
+    res.status(200).json(success('OK', { data: 'Poll added' }, res.statusCode));
   } catch (err) {
-    res.status(500).send(err);
+    res.status(500).json(error('Internal Server Error', res.statusCode));
   }
 });
 
@@ -38,21 +40,24 @@ router.delete('/:id', async (req, res) => {
   try {
     const poll = await pollModel.findByIdAndDelete(req.params.id);
     if (!poll) res.status(404).send('No item found');
-    res.status(200).send();
+    res.status(200).json(success('OK', { data: 'Poll Deleted' }, res.statusCode));
   } catch (err) {
     res.status(500).send(err);
   }
 });
 
 router.patch('/:id', async (req, res) => {
+  logger.info('editing');
+  logger.info(JSON.parse(req.body.options));
   try {
     const poll = { name: req.body.name, options: JSON.parse(req.body.options) };
+    logger.info(poll);
     poll.edited = true;
-    await pollModel.findByIdAndUpdate(req.params.id, poll);
-    await pollModel.save();
-    res.send({});
+    const response = await pollModel.findByIdAndUpdate(req.params.id, poll);
+    await response.save();
+    res.status(200).json(success('OK', { data: 'Poll Edited' }, res.statusCode));
   } catch (err) {
-    res.status(500).send(err);
+    res.status(500).json(error('Internal Server Error', res.statusCode));
   }
 });
 
